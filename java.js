3bd74c059836287579995062730d16a06d4d3b69
Oskar Terstad <oskar@terstad.se>
2026-04-28 14:08:18 +0200

    Har påbörjat sidan där man kan se olika sorters bilar. Är klar med bokningssidan och godkännande av information. (Om tid över efter allt är klart, fixa godkännande av bokning genom email)

--- java.js
+++ java.js
@@ -1,72 +1,49 @@
 const pageId = document.body.id;
 
-switch (pageId){
-    case 'index.html':
+console.log("test: " + pageId);
 
+switch (pageId){
+    case 'index.html': {
+        
         //Button direkt till bokning
         document.getElementById("bookingButton")?.addEventListener("click", () =>{
-            window.location.href = "book.html"
+            window.location.href = "book.html";
         })
 
         //Button till bokning
         document.getElementById("bookingButton_1")?.addEventListener("click", () =>{
-            window.location.href = "book.html"
+            window.location.href = "book.html";
         })
+    break;
+        }
 
+    case 'book.html': {
+        
         //Kod för drop button
         const dropBtn = document.querySelector(".dropbtn");
         const dropContent = document.querySelector(".drop_down");
 
         if (dropBtn){
             dropBtn.onclick = function() {
-            dropContent.classList.toggle("show");
+                dropContent.classList.toggle("show");
+            }
         }
-    }
         window.onclick = function(event){
             if (!event.target.matches("#dropBtn")){
                 if (dropContent && dropContent.classList.contains("show")){
-                    dropContent.classList.remove("show");
+            dropContent.classList.remove("show");
                 }
             }
-    }
+        }
 
         function showAlert(page){
-        console.log("Du har klickat på " + page);
-        window.location.href = page
-    }
-    break;
-    case 'book.html':
-        
-    break;
-}
-
-//Kod för drop button
-const dropBtn = document.querySelector(".dropbtn");
-const dropContent = document.querySelector(".drop_down");
-
-if (dropBtn){
-    dropBtn.onclick = function() {
-        dropContent.classList.toggle("show");
-    }
-}
-window.onclick = function(event){
-    if (!event.target.matches("#dropBtn")){
-        if (dropContent && dropContent.classList.contains("show")){
-            dropContent.classList.remove("show");
+            console.log("Du har klickat på " + page);
+            window.location.href = page
         }
-    }
-}
 
-function showAlert(page){
-    console.log("Du har klickat på " + page);
-    window.location.href = page
-}
+        //Denna kod är för att spara och skicka informationen till bekräftningssidan
 
-
-
-//Denna kod är för att spara och skicka informationen till bekräftningssidan
-//Detta fungerar inte behöver lagas
-const form = document.querySelector('form');
+        const form = document.querySelector('form');
 
             form.addEventListener('submit', (e) =>{
                 console.log("test")
@@ -82,17 +59,119 @@ const form = document.querySelector('form');
 
                 window.location.href = 'godkann_info.html';
             })
+    break;
+        }
 
-
-            const SparadData = localStorage.getItem('Användarval');
+    case "godkann_info.html": {
+        
+        const SparadData = localStorage.getItem('Användarval');
             
             if (SparadData){
-                const data = SparadData;
+                const data = JSON.parse(SparadData);
 
                 console.log(data);
 
-                document.getElementById('visningsnamn').textContent = data.fname;
+
+                //Här är all kod för att skriva ut informationen i html
+                //Man hämtar först id och sen sätter du id till värdet från t.ex data.fname
+
+                if (data.fname == "" || data.lname == "" || data.email == "" || data.telnumb == "" || data.hämtdatum == "" || data.lämndatum == "" || data.hämttid == "" || data.lämntid == "" || data.bil == ""){
+                    
+                }
+                document.getElementById('fname').innerHTML = data.fname;
+                document.getElementById('lname').innerHTML = data.lname;
+                document.getElementById('email').innerHTML = data.email;
+                document.getElementById('telnumb').innerHTML = data.telnumb;
+                document.getElementById('hämtdatum').innerHTML = data.hämtdatum;
+                document.getElementById('lämndatum').innerHTML = data.lämndatum;
+                document.getElementById('hämttid').innerHTML = data.hämttid;
+                document.getElementById('lämntid').innerHTML = data.lämntid;
+                document.getElementById('bil').innerHTML = data.bil;
+
+                if (data.extraval1 == "on"){
+                    document.getElementById('extraval1').textContent = "Ja";
+                }    
+                else{
+                    document.getElementById('extraval1').textContent = "Nej"
+                }
+
+                if (data.extraval2 == "on"){
+                    document.getElementById('extraval2').textContent = "Ja";
+                }  
+                else{
+                    document.getElementById('extraval2').textContent = "Nej"
+                }   
+                
+                if (data.extraval3 == "on"){
+                    document.getElementById('extraval3').textContent = "Ja";
+                }
+                else{
+                    document.getElementById('extraval3').textContent = "Nej"
+                }
+
+                if (data.extraval4 == "on"){
+                    document.getElementById('extraval4').textContent = "Ja";
+                }
+                else{
+                    document.getElementById('extraval4').textContent = "Nej"
+                }
             }
             else{
                 document.getElementById("data").innerHTML = '<p>Ingen data hittades, gå tillbaka och fyll i formuläret</p>';
             }
+
+            //Kod för drop button
+        const dropBtn = document.querySelector(".dropbtn");
+        const dropContent = document.querySelector(".drop_down");
+
+        if (dropBtn){
+            dropBtn.onclick = function() {
+                dropContent.classList.toggle("show");
+            }
+        }
+        window.onclick = function(event){
+            if (!event.target.matches("#dropBtn")){
+                if (dropContent && dropContent.classList.contains("show")){
+            dropContent.classList.remove("show");
+                }
+            }
+        }
+
+        function showAlert(page){
+            console.log("Du har klickat på " + page);
+            window.location.href = page
+        }
+
+
+        //Kod för att godkänna informationen
+
+        document.getElementById('godkann_button').addEventListener('click', () =>{
+            console.log('test av knapp för bekräftning av information.')
+        })
+    break;
+    }
+    case 'cars.html':{
+        //Kod för drop button
+        const dropBtn = document.querySelector(".dropbtn");
+        const dropContent = document.querySelector(".drop_down");
+
+        if (dropBtn){
+            dropBtn.onclick = function() {
+                dropContent.classList.toggle("show");
+            }
+        }
+        window.onclick = function(event){
+            if (!event.target.matches("#dropBtn")){
+                if (dropContent && dropContent.classList.contains("show")){
+            dropContent.classList.remove("show");
+                }
+            }
+        }
+
+        function showAlert(page){
+            console.log("Du har klickat på " + page);
+            window.location.href = page
+        }
+        break;
+        }
+}
